449b354d15e67b326f277230dd373ad090ac9f4c
shmykovandrey <shmykovandrey88@gmail.com>
2022-07-23 21:29:59 +0300

    fix: airbnb on angular new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export default class AppComponent {
   title = 'youtube-angular';
+
+  a = 5;
 }
