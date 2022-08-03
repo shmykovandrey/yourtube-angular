68ab20f998c6e8c88de54e43b1498b54a833c2e9
shmykovandrey <shmykovandrey88@gmail.com>
2022-08-03 16:01:59 +0300

    feat: add base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import PageNotFoundComponent from './page-not-found/page-not-found.component';
+import SearchResultsBlockComponent from './youtube/components/search-results-block/search-results-block.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '404',
+    component: PageNotFoundComponent,
+  },
+  { path: '', component: SearchResultsBlockComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export default class AppRoutingModule { }
+export default class AppRoutingModule {}
